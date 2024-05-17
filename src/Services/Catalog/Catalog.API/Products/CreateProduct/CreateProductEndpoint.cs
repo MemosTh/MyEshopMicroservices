5af66ae95ece0d9365cbabaf1b94f851876c87db
agamemnon theodorakopoulos <memos.t96@gmail.com>
2024-05-17 10:18:45 +0300

    add get products endpoint

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,4 @@
-﻿using BuildingBlocks.CQRS;
-
-
+﻿
 namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
